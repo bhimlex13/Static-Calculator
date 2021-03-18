53809d1012e453c7a3413878cd6105e8c31bf1ee
Glitch (hello-webpage) <none>
2021-03-18 09:28:16 +0000

    🏡🏘 Checkpoint ./script.js:29750516/1966

--- script.js
+++ script.js
@@ -4,4 +4,36 @@
 
 // Create a variable and select all of the 'btn-numbers' elements
 
-// Create a 
+// Create a variable and select the 'btn-add' element
+
+// Create a variable and select the 'btn-subtract' element
+
+// Create a variable and select the 'btn-multiply' element
+
+// Create a variable and select the 'btn-divide' element
+
+// Create a variable and select the 'btn-equal' element
+
+// Create a variable and select the 'btn-decimal' element
+
+// Create a variable and select the 'btn-clear-all' element
+
+// Create a variable and select the 'btn-backspace' element
+
+// Add an 'click' event listener to all of the 'btn-numbers' by traversing them by 'forEach' statement
+// the 'onclick' event should retrieve 'textContent' property of the current button
+// and append it to the current value of 'txt-input-display'
+
+// Create a 'click' event to add the two operands and display the result
+
+// Create a 'click' event to subtract the two operands and display the result
+
+// Create a 'click' event to multiply the two operands and display the result
+
+// Create a 'click' event to divide the two operands and display the result
+
+// Create a 'click' event to clear the input display
+
+// Create a 'click' event to delete the last number from the operand
+
+// Create a 'click' event to append a decimal character to the operand
