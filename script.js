95545ea5f95edc58aa6810d2eebf3e7ff8c218e6
Glitch (hello-webpage) <none>
2021-03-18 10:17:00 +0000

    💫⏱ Checkpoint ./script.js:29751439/418:29751456/153:29751481/79 ./index.html:29751439/45

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
 // Create variables for the 'first operand', 'second operand', and the operation
   let firstOperand, secondOperand, operation;
 // Create a variable and select the 'txt-input-display' element
-let txtInput = document.querySelector();
+let txtInput = document.querySelector('#txt-input-display');
 // Create a variable and select all of the 'btn-numbers' elements
-let allBtns = document.querySelectorAll();
+let allBtns = document.querySelectorAll('.btn-numbers');
 // Create a variable and select the 'btn-add' element   // alvin
-let btnAdd = document.querySelector()
-
-
-
-
+let btnAdd = document.querySelector('#btn-add');
 
 // Create a variable and select the 'btn-subtract' element
   let btnSubtract = document.querySelector('#btn-subtract');
 // Create a variable and select the 'btn-multiply' element
   let btnMultiply = document.querySelector('#btn-multiply');
 // Create a variable and select the 'btn-divide' element
-  let btnDivide = document.querySelector('#btn-multiply');
+  let btnDivide = document.querySelector('#btn-divide');
 // Create a variable and select the 'btn-equal' element /alex
+  let btnEqual = document.querySelector('btn-equal');
 
 
-
-
-
-
-// Create a variable and select the 'btn-decimal' element
-    let btnDecimal = document.querySelector('#btn-decimal')
-// Create a variable and select the 'btn-clear-all' element
-    let btnClearAll = document.querySelector('#btn-clear-all')
-// Create a variable and select the 'btn-backspace' element // booker
-    let btnBackspace = 
-
-
-
-
+// Create a variable and select the 'btn-decimal' element // 
+  let btnDecimal = document.querySelector('#btn-decimal')
+// Create a variable and select the 'btn-clear-all' element // 
+  let btnClearAll = document.querySelector('#btn-clear-all')
+// Create a variable and select the 'btn-backspace' element // Booker
+  let btnBackspace = document.querySelector('#btn-backspace')
 
 
 // Add an 'click' event listener to all of the 'btn-numbers' by traversing them by 'forEach' statement
 // the 'onclick' event should retrieve 'textContent' property of the current button
 // and append it to the current value of 'txt-input-display'
+  allBtns.forEach( btn => {
+   console.log(btn.textContent);
+});
+
 
 // Create a 'click' event to add the two operands and display the result
 
