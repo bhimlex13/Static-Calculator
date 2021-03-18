9cb7705dc86f27d7b656ab056ef4860694f93512
Glitch (hello-webpage) <none>
2021-03-18 09:16:23 +0000

    🌄💫 Checkpoint ./index.html:29750516/3546 ./style.css:29750516/769 ./script.js:29750516/532

---
+++ script.js
@@ -0,0 +1,7 @@
+// Create variables for the 'first operand', 'second operand', and the operation
+
+// Create a variable and select the 'txt-input-display' element
+
+// Create a variable and select all of the 'btn-numbers' elements
+
+// Create a 
